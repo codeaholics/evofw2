273bb8af1d48681090d980806ff162242226b902
Danny Yates <danny@blackcomb.co.uk>
2017-04-04 08:40:43 +0100

    Refactor LED code to be in one place

--- evo.c
+++ evo.c
@@ -7,22 +7,11 @@
 #include "led.h"
 #include "transcoder.h"
 
-ISR(TIMER1_COMPA_vect) {
-  led_toggle();
-}
-
 void main_init(void) {
   // OSCCAL=((uint32_t)OSCCAL * 10368) / 10000;
 
   wdt_disable();
-
   led_init();
-  led_on();
-
-  // One second time to blink LED
-  OCR1A = (F_CPU / 1024) - 1;
-  TCCR1B |= (1 << WGM12) | (1 << CS10) | (1 << CS12);
-  TIMSK1 = (1 << OCIE1A);
 
   // Wire up components
   transcoder_init(&tty_write_str, &driver_send_byte);
--- led.c
+++ led.c
@@ -1,3 +1,4 @@
+#include <avr/interrupt.h>
 #include <avr/io.h>
 #include "led.h"
 
@@ -5,8 +6,19 @@
 #define LED_PORT  PORTC
 #define LED_PIN   6
 
+ISR(TIMER1_COMPA_vect) {
+  led_toggle();
+}
+
 inline void led_init() {
   LED_DDR |= (1 << LED_PIN);
+
+  led_on();
+
+  // One second time to blink LED
+  OCR1A = (F_CPU / 1024) - 1;
+  TCCR1B |= (1 << WGM12) | (1 << CS10) | (1 << CS12);
+  TIMSK1 = (1 << OCIE1A);
 }
 
 inline void led_on() {
